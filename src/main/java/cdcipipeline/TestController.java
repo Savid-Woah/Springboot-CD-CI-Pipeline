aadc9db3387645b9ef5d713826f1356a925c92c8
sballesteros <sballesteros@tessi.co>
2023-12-10 17:11:09 -0500

    something added

--- src/main/java/cdcipipeline/TestController.java
+++ src/main/java/cdcipipeline/TestController.java
@@ -1,15 +1,17 @@
 package cdcipipeline;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @ResponseBody
 @RequiredArgsConstructor
-@RequestMapping("test/")
+@RequestMapping("/")
 public class TestController {
+
+    @GetMapping
+    public String imHealthy(){
+        return "Im healthy";
+    }
 }
